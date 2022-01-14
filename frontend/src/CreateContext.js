92e1931083fadc55a2119a40c40674e5cc17a443
nilesh-dhangar20 <nilesh45155@gmail.com>
2022-01-14 20:43:33 +0530

    first commit

---
+++ frontend/src/CreateContext.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import App from './App'
+const CreateContext = () => {
+    return (
+        <>
+            <App/>
+        </>
+    )
+}
+
+export default CreateContext
